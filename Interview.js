cb114e9a475730e921605a4fbf4551a24351d885
karlcode <karl.l1@hotmail.com>
2018-02-08 17:40:17 +1100

    Parse method on array to become csv ready

--- Interview.js
+++ Interview.js
@@ -10,7 +10,7 @@ considerations --
 
 
 function triggerValidation(el) {
-  var regex = new RegExp("(.*?)\.(csv)$");
+  let regex = new RegExp("(.*?)\.(csv)$");
   if (!(regex.test(el.value.toLowerCase()))) {
     el.value = '';
     alert('Please select a .csv file');
@@ -18,100 +18,110 @@ function triggerValidation(el) {
 }
 
 window.onload = function() {
-    var fileInput = document.getElementById('fileInput');
-    var fileDisplayArea = document.getElementById('fileDisplayArea');
-    var csvArray = []
-    fileInput.addEventListener('change', function(e) {
-        var file = fileInput.files[0];
-        var reader = new FileReader();
-        reader.onload = function(event) {
-            var text = event.target.result
-            fileDisplayArea.innerHTML = text
-            csvArray = csvToArray(text)
-            console.log(parseArray(csvArray))
-        };
-        reader.readAsText(file);
-    })
+  let fileInput = document.getElementById('fileInput');
+  let fileDisplayArea = document.getElementById('fileDisplayArea');
+  let csvArray = []
+  fileInput.addEventListener('change', (e) => {
+    let file = fileInput.files[0];
+    let reader = new FileReader();
+    reader.onload = function(event) {
+      let text = event.target.result
+      fileDisplayArea.innerHTML = text
+      csvArray = csvToArray(text)
+      let parsedArray = parseArray(csvArray)
+      convertToCSV(parsedArray)
+    };
+    reader.readAsText(file);
+  })
 }
 
 function csvToArray(csv) {
-    var lines = csv.split(/\r\n|\n|\r/);
-    var result = [];
-    //split first line into headers
-    var headers = lines[0].split(",");
-    //for the proceeding lines i.e i=1
-    for (var i = 1; i < lines.length - 1; i++) {
-      var obj = {};
-      var currentline = lines[i].split(",");
-      for (var j in headers){
-        if (currentline[j]) {
-            if(currentline[j].match(/[a-z]/i)){
-                obj[headers[j]] = currentline[j]
-            } else {
-                obj[headers[j]] = parseFloat(currentline[j]);
-            } 
-        }
+  let lines = csv.split(/\r\n|\n|\r/);
+  let result = [];
+  let headers = lines[0].split(",");
+  for (let i = 1; i < lines.length - 1; i++) {
+    let obj = {};
+    let currentline = lines[i].split(",");
+    for (header in headers){
+      if (currentline[header]) {
+        if(currentline[header].match(/[a-z]/i)){
+          obj[headers[header]] = currentline[header]
+        } else {
+          obj[headers[header]] = parseFloat(currentline[header]);
+        } 
       }
-      result.push(obj);
     }
-    return result;
+    result.push(obj);
+  }
+  return result;
 }
 
 
 function parseArray(array){
-    var sortedArray = array.sort((a, b) => {
-        return a['Origin Year'] - b['Origin Year']  ||  a['Development Year']- b['Development Year'];
-        })
-    var lowestYear = sortedArray[0]['Origin Year']
-    var highestYear = sortedArray[sortedArray.length - 1]['Origin Year']
-    const uniqueProduct = [...new Set(sortedArray.map(key => key['Product']))];
-    var cache = uniqueProduct.reduce((obj, v) => {
-        obj[v] = {}
-        var ref = obj[v]
-        for (var x = lowestYear; x <= highestYear; x++) {
-            ref[x] = [];
-          }
-        return obj
-    }, {})
+  let sortedArray = array.sort((a, b) => {
+    return a['Origin Year'] - b['Origin Year']  ||  a['Development Year']- b['Development Year'];
+    })
+  let lowestYear = sortedArray[0]['Origin Year']
+  let highestYear = sortedArray[sortedArray.length - 1]['Origin Year']
+  const uniqueProduct = [...new Set(sortedArray.map(key => key['Product']))];
+  let cache = uniqueProduct.reduce((obj, v) => {
+    obj[v] = {}
+    let ref = obj[v]
+    for (let x = lowestYear; x <= highestYear; x++) {
+      ref[x] = [];
+      }
+    return obj
+  }, {})
 
-    for (var i = 0; i < sortedArray.length; i++){
-        var row = sortedArray[i]
-        var yearIndex = row['Development Year'] - row['Origin Year']
-        var product = row['Product']
-        var origin = row['Origin Year']
-        var increment = row['Incremental Value']
-        for (var j = 0; j < uniqueProduct.length; j++){
-            if (uniqueProduct[j] == product){
-                cache[uniqueProduct[j]][origin][yearIndex] = increment
-            } else {
-                cache[uniqueProduct[j]][origin][yearIndex] = 0
-            }
-        }  
-    }
-    for (key in cache){
-        for (year in cache[key]){
-            for (var y = 0; y < cache[key][year].length; y++){
-                var index = cache[key][year]
-                if (typeof index[y] == 'undefined'){ 
-                    index[y] = index[y-1]
-                } else if ( index[y] < index[y-1]) {
-                    index[y] += index[y-1]
-                }
-            }
-        }
+  for (let i = 0; i < sortedArray.length; i++){
+    let row = sortedArray[i]
+    let yearIndex = row['Development Year'] - row['Origin Year']
+    let product = row['Product']
+    let origin = row['Origin Year']
+    let increment = row['Incremental Value']
+    for (let j = 0; j < uniqueProduct.length; j++){
+      if (uniqueProduct[j] == product){
+        cache[uniqueProduct[j]][origin][yearIndex] = increment
+      } else {
+        cache[uniqueProduct[j]][origin][yearIndex] = 0
+      }
     }  
-    console.log(cache);
-    console.log(sortedArray);
-}
-/*()
-var num = 1
-while (num < yearIndex + 1 ){
-            
-    if (!cache[product][origin][num]){
-        cache[product][origin][num] = cache[product][origin][num - 1]
-        cache[product][origin][num]
+  }
+  for (key in cache){
+    for (year in cache[key]){
+      for (let y = 0; y < cache[key][year].length; y++){
+        let index = cache[key][year]
+        if (typeof index[y] == 'undefined'){ 
+          index[y] = index[y-1]
+        } else if ( y > 0) {
+          index[y] += index[y-1]
+        }
+      }
     }
-    num++
-    
-    
-} */
+  }  
+  return cache
+}
+
+function convertToCSV(objArray) {
+  let firstYear
+  let lastYear
+  let parsedData = []
+  for (product in objArray){
+    let set = []
+    Object.keys(objArray[product]).forEach((key, index, array) => {
+      if (index === 0){
+        firstYear = key
+      } else if (index === array.length - 1){
+        lastYear = key
+      }
+      set.push(objArray[product][key])
+    });
+    set.unshift(product)
+    let merged = [].concat.apply([], set);
+    parsedData.push(merged)
+  }
+  parsedData.unshift([firstYear, lastYear - firstYear + 1])
+  parsedData = parsedData.join('\r\n')
+  console.log(parsedData);
+
+}
